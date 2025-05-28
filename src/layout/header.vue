bebdf4a1cf404a61eebcf4998397db854ba0d9c7
demigods <a13752365641@126.com>
2025-05-28 09:46:48 +0800

    upgrade:将一级菜单修改为动态渲染。

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -2,22 +2,26 @@
   <a-layout-header theme="dark">
     <div class="logo" />
     <a-menu theme="dark" mode="horizontal" v-model:selected-keys="selectedKeys">
-      <a-menu-item key="1">一级菜单1</a-menu-item>
-      <a-menu-item key="2">一级菜单2</a-menu-item>
-      <a-menu-item key="3">一级菜单3</a-menu-item>
+      <!-- 动态渲染菜单项 -->
+      <a-menu-item v-for="item in menuItems" :key="item.key">{{ item.label }}</a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
 
 <script lang="ts" setup>
-// 导入响应式工具
 import { ref } from 'vue';
-// 导入响应式类型
-import type { Ref } from 'vue';
+
+import { MenuItem } from '@/interface/menu.interface';
+
+// 定义菜单列表
+const menuItems: MenuItem[] = [
+  { key: '1', label: '一级菜单1' },
+  { key: '2', label: '一级菜单2' },
+  { key: '3', label: '一级菜单3' }
+];
 
 // 定义菜单选中项
-const selectedKeys:Ref<string[]> = ref(['1']);
-
+const selectedKeys = ref<string[]>(['1']);
 </script>
 
 <style lang="less">
