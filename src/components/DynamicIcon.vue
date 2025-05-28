5f1606eee1653a9d00ad81dd22f1031176380530
demigods <a13752365641@126.com>
2025-05-28 13:54:10 +0800

    feat:添加动态图标组件。

---
+++ src/components/DynamicIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="iconComponent" />
+</template>
+
+<script lang="ts" setup>
+// 引入vue
+import { computed } from 'vue';
+// 引入图标
+import * as icons from '@ant-design/icons-vue';
+
+// 接收图标名称
+const props = defineProps<{
+  iconName: string;
+}>();
+
+// 动态获取图标组件
+const iconComponent = computed(() => {
+  return icons[props.iconName] || null;
+});
+</script>
