a5fa07df05a3df5fb5736a9146586de51cc99422
demigods <a13752365641@126.com>
2025-05-30 13:42:25 +0800

    upgrade:添加路由元信息。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHashHistory,  } from 'vue-router'
-import type {RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 
 import Login from '@/views/login.vue';
 import Home from '@/views/home.vue';
@@ -13,37 +13,37 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: Home,
-    meta: { requiresAuth: true } // 需要布局组件
+    meta: { requiresAuth: true, title: '首页', icon: 'home-icon' } // 添加图标和标题
   },
   {
     path: '/login',
     name: 'Login',
     component: Login,
-    meta: { requiresAuth: false } // 不需要布局组件
+    meta: { requiresAuth: false, title: '登录', icon: 'login-icon' } // 添加图标和标题
   },
   {
     path: '/unknown',
     name: 'unknown',
     component: ErrorUnknown,
-    meta: { requiresAuth: false } // 不需要布局组件
+    meta: { requiresAuth: false, title: '未知错误', icon: 'error-icon' } // 添加图标和标题
   },
   {
     path: '/500',
     name: '500',
     component: Error500,
-    meta: { requiresAuth: false } // 不需要布局组件
+    meta: { requiresAuth: false, title: '服务器错误', icon: 'error-icon' } // 添加图标和标题
   },
   {
     path: '/403',
     name: '403',
     component: Error403,
-    meta: { requiresAuth: false } // 不需要布局组件
+    meta: { requiresAuth: false, title: '禁止访问', icon: 'error-icon' } // 添加图标和标题
   },
   {
     path: '/:catchAll(.*)', // 匹配所有未定义的路径
     name: '404',
     component: Error404,
-    meta: { requiresAuth: false } // 不需要布局组件
+    meta: { requiresAuth: false, title: '页面未找到', icon: 'error-icon' } // 添加图标和标题
   },
 ];
 
